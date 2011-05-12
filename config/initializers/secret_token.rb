67b37236fcf7f517baff2b7a76807136faded235
Johnny A. Prioleau <jprioleau83@gmail.com>
2011-05-12 19:28:44 -0400

    restart

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Weeklies::Application.config.secret_token = '7ae625c002f7d5a0a1e4208d771c078ee79daf9a8d65b98f1e54cebb02415a53c42d67461ba49326ce922cb69ea3fbcd0b4e6992ccba246ca8919bb6cf3432a8'
